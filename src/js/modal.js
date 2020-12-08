0ebda15dc040a0339daaf1f96bb2026221ca99f2
VladislavVB <burdukovskij.vlad@mail.ru>
2020-12-08 12:20:00 +0300

    modal add

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,45 +1,9 @@
-let modal,
-    isModal = false;
-window.onload = () => {
-    hiddenModal = document.querySelectorAll('.modal-visible');
-}
-const showAllCards = () => {
-    if (isModal) {
-        // document.querySelector('.portfolio-button').innerHTML = 'Explore All'
-        hiddenModal.forEach(element => {
-            element.classList.add('modal-visible');
-        });
-        isModal = false;
-    } else {
-        // document.querySelector('.portfolio-button').innerHTML = 'Close'
-        hiddenModal.forEach(element => {
-            element.classList.remove('modal-visible');
-        });
-        isModal = true;
-    }
+let openModal = () => {
+    document.querySelector('.modal').classList.add('modal-visible');
+    console.log(1);
 }
 
-// document.getElementById('intro_btn').onclick = function() {
-//     document.getElementById('main').classList.add('modal-visible');
-//   }
-// $(document).ready(function () {
-//     var modalForm = $('.modal__form');
-//     var controlForm = $('.control__form');
-//     var footerForm = $('.footer__form');
-    
-//     onSubmitForm(modalForm)
-//     onSubmitForm(controlForm)
-//     onSubmitForm(footerForm)
+let closeModal = () => {
+    document.querySelector('.modal').classList.remove('modal-visible');
+}
 
-//     $('[data-toggle=modal]').on('click', function () {
-//         modal.toggleClass('modal--visible');
-//     });
-
-//     $('.modal__close').on('click', function () {
-//         modal.toggleClass('modal--visible');
-//     });
-
-//     $('.modal-up__close').on('click', function () {
-//         modalUp.removeClass('modal-up--visible');
-//     });
-// });
